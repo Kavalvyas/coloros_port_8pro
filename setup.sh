5e1564cbac0e4544323ca48d06fb29c321f24480
Bruce Teng <torai.d.l@outlook.com>
2025-05-06 16:21:40 +0800

    Update: Java version to 21

--- setup.sh
+++ setup.sh
@@ -11,9 +11,9 @@ if [ "$(uname -m)" == "x86_64" ] && [  "$(uname)" == "Linux" ];then
     echo "Device arch: Linux x86_64"
     apt update -y
     apt upgrade -y
-    apt install -y aria2 python3 busybox zip unzip p7zip-full openjdk-8-jre zipalign zstd bc android-sdk-libsparse-utils xmlstarlet
+    apt install -y aria2 python3 busybox zip unzip p7zip-full openjdk-21-jre zstd bc android-sdk-libsparse-utils xmlstarlet
     if [ $? -ne 0 ];then
-        echo "安装可能出错，请手动执行：apt install -y aria2 python3 busybox zip unzip p7zip-full openjdk-8-jre zipalign zstd bc xmlstarlet"
+        echo "安装可能出错，请手动执行：apt install -y aria2 python3 busybox zip unzip p7zip-full openjdk-21-jre zstd bc xmlstarlet"
     fi
 fi
 
@@ -21,7 +21,7 @@ if [ "$(uname -m)" == "aarch64" ];then
     echo "Device arch: aarch64"
     apt update -y
     apt upgrade -y
-    apt install -y python busybox zip unzip p7zip openjdk-17 zipalign zstd xmlstarlet
+    apt install -y python busybox zip unzip p7zip openjdk-21 zipalign zstd xmlstarlet
 fi
 
 if [ "$(uname)" == "Darwin" ] && [ "$(uname -m)" == "x86_64" ];then
